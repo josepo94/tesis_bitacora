2fe45394e33372214e55e3d83812fac998549396
jospo94 <josebas-94@hotmail.com>
2019-06-05 00:31:24 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacionFechas',
+  templateUrl: 'pantallas/app.Fechas.html'
+})
+export class AppComponent {
+  title = 'bitacora-app';
+}
