c7b56e432d30f9b5afddf77be64ad292ab857633
jospo94 <josebas-94@hotmail.com>
2019-06-05 23:18:47 -0500

    cambios realizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-verificacionFechas',
-  templateUrl: 'pantallas/app.Fechas.html'
+  templateUrl: 'pantallas/app.VerificacionPozoProductor.html'
 })
 export class AppComponent {
-  title = 'bitacora-app';
+
 }
